4518dc602cc1c40d7f07000a8b1db649d3c30cb7
Kharboush98 <raafatkharboush@gmail.com>
2026-06-01 16:42:57 +0300

    Fixed reload issue in AuthContent so that it doesn't redirect back to homepage when reloading from another!

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -5,15 +5,18 @@ export const AuthContext = createContext()
 
 export default function AuthContextProvider({children}) {
 
-  const [token , setToken] = useState(null);
+//   const [token , setToken] = useState(null);
 
-  const userToken = localStorage.getItem("userToken");
+//   const userToken = localStorage.getItem("userToken");
 
-  useEffect(() => {
-      if(userToken){
-          setToken(userToken);
-      }
-  }, []);
+//   useEffect(() => {
+//       if(userToken){
+//           setToken(userToken);
+//       }
+//   }, []);
+
+    const [token, setToken] = useState(localStorage.getItem("userToken")); // ← initialize directly, no useEffect needed
+
 
   return <AuthContext.Provider value={{token , setToken}}>{children}</AuthContext.Provider>
 }
