b171c0bfed1c9e61fe74f335465923f3551b2151
Kharboush98 <raafatkharboush@gmail.com>
2026-06-01 17:04:20 +0300

    added Scroll to the top functionality when navigating betweeen pages for a better look!

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import NavbarComponent from '../../Component/Navbar/Navbar'
 import { Outlet } from 'react-router'
 import Footer from '../../Component/Footer/Footer'
+import ScrollToTop from '../../Functions/Scroll/ScrollToTop'
 
 export default function MainLayout() {
   return (
     <>
+      <ScrollToTop/>
       <NavbarComponent/>
       <Outlet/>
       {/* <Footer/> */}
